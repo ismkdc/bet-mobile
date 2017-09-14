6f2384cb814419cbe5909dc15de6eb076b625ca4
Ismail Kundakci <ismailkundakci@DESKTOP-MBTEQ43>
2017-09-14 15:05:21 +0300

    added something

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  bets: any;
+  constructor(public navCtrl: NavController , public http: Http) {
+    this.http.get('http://ismkdcssh.ismailkundakci.com:5000/values').subscribe(data => {
+      this.bets = data.json();
+  });
   }
 
 }
